824cf987896b1f0a256f8d6e0137d316dd6d225c
avnish1981 <avnish.choubey@gmail.com>
2020-05-11 15:36:19 +0530

    first commit

--- LamdaExprssionApp/Program.cs
+++ LamdaExprssionApp/Program.cs
@@ -6,10 +6,30 @@ using System.Threading.Tasks;
 
 namespace LamdaExprssionApp
 {
+   /* => is called lamda operator and read as Goes TO. Notice that within lamda expression you don't have to use delegate 
+    keyword explicity and you don't have to specify the input parameter type explicity. */
     class Program
     {
         static void Main(string[] args)
         {
+            List<Employee> listemp = new List<Employee>
+            {
+                new Employee{eid=101,ename="Avnish"},
+                 new Employee{eid=102,ename="Manish"},
+                  new Employee{eid=103,ename="Sohan"},
+                   new Employee{eid=104,ename="Kanita"}
+                   
+
+            };
+
+            LamdaExprssionApp.Employee emp = listemp.Find(empl => empl.eid == 102); // => read as "GOES TO "
+            Console.WriteLine("Eid :{0} and Name :{1}", emp.eid, emp.ename);
         }
+    }
+
+    public class Employee
+    {
+        public int eid { get; set; }
+        public string ename { get; set; }
     }
 }
